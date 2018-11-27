0a3b10bffeb12b5e632781338c0af2b8bc19e2c0
Carter Carlson <carterbud1@gmail.com>
2018-11-27 15:35:45 -0600

    Modularized code, added tracker for invalid product ID's

--- scrape.py
+++ scrape.py
@@ -14,7 +14,6 @@ Summary
 |  ASIN  |  reviews_verified  |  reviews_unverified  |  avg_review  |  avg_review_verified  |  avg_review_unverified  |
 
 '''
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
@@ -24,43 +23,19 @@ from datetime import datetime
 from time import sleep
 import random
 
-df = pd.read_excel('ASINS_to_scrape.xlsx')
-df1 = []
-df2 = []
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('log-level=3')
-driver = webdriver.Chrome('chromedriver.exe', options=chrome_options)
+def main():
 
-widgets = ['ASIN ', SimpleProgress(),'  ', Percentage(), ' ', Bar(marker='#',left='[',right=']'), ' ', ETA()]
-pbar = ProgressBar(widgets=widgets, maxval=len(df)).start()
-
-for i, asin in enumerate(df['ASIN']):
-	pbar.update(i + 1)
-	# Pull reviews
-	reviews_link = 'https://www.amazon.com/product-reviews/' + asin + '/ref=acr_search_see_all?ie=UTF8&showViewpoints=1'
-	driver.get(reviews_link)
-
-	last_page = False
-	while not last_page:
+	def pull_reviews(prod_code):
 		sleep(1 + random.random())
-		#sleep(random.random())
 		reviews = driver.find_elements_by_xpath("//div[@id='cm_cr-review_list']/div")
-		if len(reviews) <= 10:
-			last_page = True
+
+		if len(reviews) == 0:
+			invalid_ASINS.append(prod_code)
+			return
+
 		for el in reviews:
 			try:
-				el.find_element_by_xpath('span').get_attribute('class') == 'a-declarative'
-				try:
-					driver.find_element_by_xpath("//li[@class='a-last']/a").click()
-				except:
-					last_page = True
-
-				break
-
-			except:
-
 				review_id = el.get_attribute('id')
 				temp = el.find_elements_by_xpath('div/div')
 				rating = temp[1]
@@ -68,8 +43,7 @@ for i, asin in enumerate(df['ASIN']):
 				rating = float(rating[:rating.find(' ')])
 				author_id = temp[0]
 				author_id = author_id.find_element_by_xpath('a').get_attribute('href')
-				author_id = author_id[author_id.find('account.')+len('account.'):author_id.find('/ref')]
-
+				author_id = author_id[author_id.find('account.') + len('account.'):author_id.find('/ref')]
 
 				data = el.text.split('\n')
 				author_name = data[0]
@@ -94,65 +68,121 @@ for i, asin in enumerate(df['ASIN']):
 
 					review = review + '  ' + data[i]
 
-				df1.append([asin, date, author_id, author_name, rating, title, review, verified])
-
-
-	# pull questions
-	questions_link = 'https://www.amazon.com/ask/questions/asin/'+ asin +'/ref=ask_dp_dpmw_ql_hza?isAnswered=true'
-	driver.get(questions_link)
-
-	while True:
-		sleep(1 + random.random())
-		questions = driver.find_elements_by_xpath("//div[@class='a-section askTeaserQuestions']/div")
-		try:
-			for q in questions:
-				date = q.find_element_by_xpath("div/div[2]/div[2]/div/div[2]/span[2]").text[2:]
-				date = datetime.strptime(date, '%B %d, %Y')
-				question = q.find_element_by_xpath("div/div[2]/div/div/div[2]/a/span").text
-				answer = q.find_element_by_xpath("div/div[2]/div[2]/div/div[2]/span[1]").text
-
-				df2.append([asin, date, question, answer])
-
-			try:
-				driver.find_element_by_xpath("//li[@class='a-last']/a").click()
+				reviews_df.append([prod_code, date, author_id, author_name, rating, title, review, verified])
 
 			except:
-				break
+				try:
+					driver.find_element_by_xpath("//li[@class='a-last']/a").click()
+					return pull_reviews(prod_code)
+				except:
+					return
+
+
+	def pull_questions(prod_code):
+		sleep(1 + random.random())
+		questions = driver.find_elements_by_xpath("//div[@class='a-section askTeaserQuestions']/div")
+
+		if len(questions) == 0:
+			return
+
+		for q in questions:
+			date = q.find_element_by_xpath("div/div[2]/div[2]/div/div[2]/span[2]").text[2:]
+			date = datetime.strptime(date, '%B %d, %Y')
+			question = q.find_element_by_xpath("div/div[2]/div/div/div[2]/a/span").text
+			answer = q.find_element_by_xpath("div/div[2]/div[2]/div/div[2]/span[1]").text
+			questions_df.append([prod_code, date, question, answer])
+
+		try:
+			driver.find_element_by_xpath("//li[@class='a-last']/a").click()
+			return pull_questions(prod_code)
 		except:
-			break
+			return
 
 
-pbar.finish()
-driver.close()
-df1 = pd.DataFrame(df1, columns=['ASIN', 'date', 'author_id', 'author_name', 'rating', 'title', 'description', 'verified'])
-df2 = pd.DataFrame(df2, columns=['ASIN', 'date', 'question', 'answer'])
+	reviews_df = []
+	questions_df = []
+	invalid_ASINS = []
 
-avg_review = df1.groupby('ASIN')['rating'].mean().tolist()
+	driver.get('https://www.amazon.com')
 
-reviews_verified = df1.loc[df1['verified'] == 1].groupby('ASIN')[['verified', 'rating']]
-num_verified = reviews_verified.count()['verified'].tolist()
-avg_review_verified = reviews_verified.mean()['rating'].tolist()
+	for x, asin in enumerate(df['ASIN']):
+		pbar.update(x + 1)
 
-reviews_unverified = df1.loc[df1['verified'] == 0].groupby('ASIN')[['verified', 'rating']]
-num_unverified = reviews_unverified.count()['verified'].tolist()
-avg_review_unverified = reviews_unverified.mean()['rating'].tolist()
+		# Pull reviews
+		reviews_link = 'https://www.amazon.com/product-reviews/' + asin + '/ref=acr_search_see_all?ie=UTF8&showViewpoints=1'
+		driver.get(reviews_link)
 
-df3 = pd.DataFrame({
-	'ASIN': df['ASIN'].tolist(),
-	'reviews_verified': num_verified,
-	'reviews_unverified': num_unverified,
-	'avg_review': avg_review,
-	'avg_review_verified': avg_review_verified,
-	'avg_review_unverified': avg_review_unverified
-})
+		pull_reviews(asin)
 
-df1.set_index('ASIN', drop=True, inplace=True)
-df2.set_index('ASIN', drop=True, inplace=True)
-df3.set_index('ASIN', drop=True, inplace=True)
+		if asin in invalid_ASINS:
+			continue
 
-df1.to_csv('data/reviews.csv')
-df2.to_csv('data/questions.csv')
-df3.to_csv('data/summary.csv')
+		questions_link = 'https://www.amazon.com/ask/questions/asin/'+ asin +'/ref=ask_dp_dpmw_ql_hza?isAnswered=true'
+		driver.get(questions_link)
 
-print('Complete!')
-sleep(5)
+		pull_questions(asin)
+
+	pbar.finish()
+	driver.close()
+	reviews_df = pd.DataFrame(reviews_df, columns=['ASIN', 'date', 'author_id', 'author_name', 'rating', 'title', 'description', 'verified'])
+	questions_df = pd.DataFrame(questions_df, columns=['ASIN', 'date', 'question', 'answer'])
+
+	avg_review = reviews_df.groupby('ASIN')['rating'].mean().tolist()
+
+	num_verified = []
+	num_unverified = []
+	avg_review_verified = []
+	avg_review_unverified = []
+
+	for asin in set(reviews_df['ASIN']):
+		temp_verified = reviews_df[(reviews_df['ASIN'] == asin) & (reviews_df['verified'] == 1)]
+		num_verified.append(temp_verified.count()['verified'])
+		avg_review_verified.append(temp_verified.mean()['rating'])
+		temp_unverified = reviews_df[(reviews_df['ASIN'] == asin) & (reviews_df['verified'] == 0)]
+		num_unverified.append(temp_unverified.count()['verified'])
+		avg_review_unverified.append(temp_unverified.mean()['rating'])
+
+
+	summary = pd.DataFrame({
+		'ASIN': list(set(reviews_df['ASIN'])),
+		'reviews_verified': num_verified,
+		'reviews_unverified': num_unverified,
+		'avg_review': avg_review,
+		'avg_review_verified': avg_review_verified,
+		'avg_review_unverified': avg_review_unverified
+	})
+
+	reviews_df.set_index('ASIN', drop=True, inplace=True)
+	questions_df.set_index('ASIN', drop=True, inplace=True)
+	summary.set_index('ASIN', drop=True, inplace=True)
+
+	try:
+		os.remove('data/reviews.csv')
+		os.remove('data/questions.csv')
+		os.remove('data/summary.csv')
+		os.remove('data/invalid_ASIN.csv')
+	except:
+		pass
+
+	reviews_df.to_csv('data/reviews.csv')
+	questions_df.to_csv('data/questions.csv')
+	summary.to_csv('data/summary.csv')
+
+	if len(invalid_ASINS) > 0:
+		print('\nUnable to pull data for {} ASINs.  Details available in invalid_ASIN.csv\n'.format(len(invalid_ASINS)))
+		pd.DataFrame({'ASIN': invalid_ASINS}).to_csv('data/invalid_ASIN.csv')
+
+if __name__ == '__main__':
+
+	df = pd.read_excel('ASINS_to_scrape.xlsx')
+
+	chrome_options = Options()
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('log-level=3')
+	driver = webdriver.Chrome('chromedriver.exe', options=chrome_options)
+
+	widgets = ['ASIN ', SimpleProgress(),'  ', Percentage(), ' ', Bar(marker='#',left='[',right=']'), ' ', ETA()]
+	pbar = ProgressBar(widgets=widgets, maxval=len(df)).start()
+
+	main()
+	print('Complete!')
